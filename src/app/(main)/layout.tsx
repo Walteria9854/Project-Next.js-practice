da5a33d6f81e2a730928e8271576caa28d1e438a
Walteria9854 <yukikatayamaaapu@gmail.com>
2024-12-03 17:00:32 +0800

    v1.1

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,16 +1,16 @@
 import SideMenu from "../components/SideMenu/SideMenu";
 
 const MainLayout = ({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) => {
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
   return (
     <div className="flex h-screen">
-        <SideMenu />
-      <main className = "bg-red-300 flex-1 overflow-auto">{children}</main>
+      <SideMenu />
+      <main className="bg-slate-50 flex-1 overflow-auto">{children}</main>
     </div>
-    );
-  };
+  );
+};
 
 export default MainLayout;
--- src/app/components/SideMenu/NavList/NavList.tsx
+++ src/app/components/SideMenu/NavList/NavList.tsx
@@ -1,11 +1,46 @@
-const NavList = () => {
-  return (
-    <div>
-        <div>Link1</div>
-        <div>Link2</div>
-        <div>Link3</div>
-    </div>
-  )
+import { FaRegCheckSquare, FaRegClock, FaTasks } from "react-icons/fa";
+import NavItem from "./NavItem/Navitem";
+
+interface NavItemType {
+  id: number;
+  label: string;
+  link: string;
+  icon: React.ReactNode;
 }
 
-export default NavList
+const NavList = () => {
+  const navList: NavItemType[] = [
+    {
+      id: 1,
+      label: "All Tasks",
+      link: "/",
+      icon: <FaTasks className="size-5" />,
+    },
+    {
+      id: 2,
+      label: "Completed Tasks",
+      link: "/completed",
+      icon: <FaRegCheckSquare className="size-5" />,
+    },
+    {
+      id: 3,
+      label: "Expired Tasks",
+      link: "/expired",
+      icon: <FaRegClock className="size-5" />,
+    },
+  ];
+  return (
+    <div className="mt-24">
+      {navList.map((item) => (
+        <NavItem
+          key={item.id}
+          label={item.label}
+          link={item.link}
+          icon={item.icon}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default NavList;
