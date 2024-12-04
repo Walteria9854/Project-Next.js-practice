df933c0d9d8abdd95c81187296fccee166a7f0c2
Walteria9854 <yukikatayamaaapu@gmail.com>
2024-12-04 13:11:54 +0800

    v1.2

---
+++ src/app/components/TaskCard/TaskEditButton/TaskEditButton.tsx
@@ -0,0 +1,16 @@
+import { FaPen } from "react-icons/fa";
+import Link from "next/link";
+
+interface TaskEditButtonProps {
+  id: string;
+}
+
+const TaskEditButton: React.FC<TaskEditButtonProps> = ({ id }) => {
+  return (
+    <Link href={`/edit/${id}`}>
+      <FaPen className="hover:text-gray-700 text-lg cursor-pointer" />
+    </Link>
+  );
+};
+
+export default TaskEditButton;
